cdcaa171cbe6256a51f7c54b384e24e787e0e401
Daleon L <daleon86@gmail.com>
2020-04-15 23:38:36 -0500

    slowly but surely

---
+++ CRv1.0/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function load_css()
+  {
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('bootstrap');
+    
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), false, 'all');
+    wp_enqueue_style('custom');
+  }
+
+add_action('wp_enqueue_scripts', 'load_css');
+
+function load_js()
+  {
+    wp_enqueue_script('jquery');
+    wp_register_script('bootstrapjs', get_template_directory_uri().'/js/bootstrapjs.min.js', 'jquery', false, true);
+    wp_enqueue_script('bootstrapjs');
+  }
+add_action('wp_enqueue_scripts', 'load_js');
---
+++ CRv1.0/template-contact.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Contact Us
+*/
+?>
+
+<?php get_header();?>
+
+<div class="container">
+  <h1>
+    <?php the_title();?>
+  </h1>
+  <div class="row">
+    <div class="col-lg-6">
+      <h3>This is where the contact form goes</h3>
+    </div>
+    <div class="col-lg-6">
+      <?php get_template_part('includes/section', 'content');?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer();?>
