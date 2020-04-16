53b4d578aca78d5e340a599250834dc29d073d4f
Daleon L <daleon86@gmail.com>
2020-04-16 11:29:36 -0500

    Simple WP

--- CRv1.0/template-contact.php
+++ CRv1.0/template-contact.php
@@ -5,19 +5,19 @@ Template Name: Contact Us
 ?>
 
 <?php get_header();?>
-
-<div class="container">
-  <h1>
-    <?php the_title();?>
-  </h1>
-  <div class="row">
-    <div class="col-lg-6">
-      <h3>This is where the contact form goes</h3>
-    </div>
-    <div class="col-lg-6">
-      <?php get_template_part('includes/section', 'content');?>
+<section class="wrapper">
+  <div class="container">
+    <h1>
+      <?php the_title();?>
+    </h1>
+    <div class="row">
+      <div class="col-lg-6">
+        <h3>This is where the contact form goes</h3>
+      </div>
+      <div class="col-lg-6">
+        <?php get_template_part('includes/section', 'content');?>
+      </div>
     </div>
   </div>
-</div>
-
+</section>
 <?php get_footer();?>
